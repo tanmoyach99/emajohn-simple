177c7c2466ae1783a0919488eddbd39b8107dc4f
tanmoytonu <tanmoyacharjee18@gmail.com>
2020-04-12 19:07:00 +0600

    first commit done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './component/Header/Header'
+
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Header;
